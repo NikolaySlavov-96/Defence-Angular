d61788bdd0b0b8f7c62727a4a2c6ca712b4284b8
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-29 13:52:30 +0300

    created api service and envirorments file

---
+++ src/app/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { environment as environmentProduct } from 'src/environments/environment';
+import { IProduct } from './shared/interfaces';
+
+
+const apiURL = environment.apiURL;
+// const apiURL = environmentProduct.apiURL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  loadProducts() {
+    return this.httpClient.get<IProduct[]>(`${apiURL}/source/products`);
+  }
+
+  loadProduct(id: number) {
+    return this.httpClient.get<IProduct>(`${apiURL}/source/product/${id}`)
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CoreModule } from './core/core.module'
+import { HttpClientModule } from '@angular/common/http';
 
+import { CoreModule } from './core/core.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
@@ -13,6 +14,7 @@ import { ProductModule } from './product/product.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     CoreModule,
     ProductModule,
   ],
