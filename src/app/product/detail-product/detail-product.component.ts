f3469de193d9d836201f2f5997fd3e710b8848d6
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-30 21:28:18 +0300

    check logged in and owner

--- src/app/product/detail-product/detail-product.component.ts
+++ src/app/product/detail-product/detail-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/api/auth.service';
 import { IProduct } from 'src/app/shared/interfaces';
 
 @Component({
@@ -11,16 +12,16 @@ export class DetailProductComponent implements OnInit {
 
   product: IProduct | null = null;
 
-  constructor(private activateRouter: ActivatedRoute, private router: Router) {
+  constructor(private activateRouter: ActivatedRoute, private router: Router, private authService: AuthService) {
     this.product = this.activateRouter.snapshot.data?.['product'][0]
   }
 
   get hasLoged() {
-    return true
+    return this.authService.isLogged;
   }
 
   get hasOwner() {
-    return true
+    return this.authService.user?._id === this.product?.owner ? true : false;
   }
 
   ngOnInit(): void {
