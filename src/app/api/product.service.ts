a0a002bbac525f18c80d0f33387d170356a84724
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-30 17:51:21 +0300

    removed and moved module

--- src/app/api/product.service.ts
+++ src/app/api/product.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { environment as environmentProduct } from 'src/environments/environment';
-import { IProduct } from './shared/interfaces';
+import { IProduct } from '../shared/interfaces';
 
 
 const apiURL = environment.apiURL;
 // const apiURL = environmentProduct.apiURL;
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+
+  product: [] | null = null;
 
   constructor(private httpClient: HttpClient) { }
 
@@ -21,5 +24,9 @@ export class ApiService {
 
   loadProduct(id: number) {
     return this.httpClient.get<IProduct>(`${apiURL}/source/product/${id}`)
+  }
+
+  createProduct(data: IProduct) {
+    return this.httpClient.post<IProduct>(`${apiURL}/source/product`, data)
   }
 }
