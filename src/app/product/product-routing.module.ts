c3c298f75c735a2258fe0ba44446416df5894c9a
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-30 22:38:53 +0300

    import component in module and create path

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -3,6 +3,7 @@ import { CreateComponent } from "./create/create.component";
 import { CatalogComponent } from "./catalog/catalog.component";
 import { DetailProductComponent } from "./detail-product/detail-product.component";
 import { ProductResolver } from "./resolvers/product.resolver";
+import { EditComponent } from "./edit/edit.component";
 
 const routes: Routes = [
     {
@@ -23,6 +24,13 @@ const routes: Routes = [
     {
         path: 'create',
         component: CreateComponent
+    },
+    {
+        path: 'edit/:id',
+        resolve: {
+            product: ProductResolver
+        },
+        component: EditComponent,
     },
 ];
 
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -6,6 +6,7 @@ import { CreateComponent } from './create/create.component';
 import { ProductRoutingModule } from './product-routing.module';
 import { CatalogComponent } from './catalog/catalog.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
+import { EditComponent } from './edit/edit.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
     CreateComponent,
     CatalogComponent,
     DetailProductComponent,
+    EditComponent,
   ],
   imports: [
     ProductRoutingModule,
