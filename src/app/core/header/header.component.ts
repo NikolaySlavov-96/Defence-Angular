92837eb1e3ffda11b0a3e3a3deb9a3751b276bed
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-30 17:57:16 +0300

    import authenticate module in header parts

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'
+import { AuthService } from 'src/app/api/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +10,13 @@ import { Router } from '@angular/router'
 export class HeaderComponent {
 
   get isLoggedIn() {
-    return false;
-  }
+    return this.authService.isLogged;
+  };
 
   get user() {
-    return { name: 'Nikolay' }
+    return this.authService.user;
   }
+  
+  constructor(private router: Router, private authService: AuthService) { }
 
-  constructor(private router: Router) { }
 }
