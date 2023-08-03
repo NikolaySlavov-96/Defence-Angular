b92515d295453294f0a6a752e73fb7ad5abe0b09
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-03 16:21:59 +0300

    create guard and not using

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from "@angular/router";
 import { RegisterComponent } from "./register/register.component";
 import { LogoutComponent } from "./logout/logout.component";
 import { LoginComponent } from "./login/login.component";
-import { AuthActivate } from "../shared/guards/auth.activate";
 
 const routes: Routes = [
     {
